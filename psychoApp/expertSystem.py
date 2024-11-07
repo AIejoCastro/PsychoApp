ff42bfb78adea0fe5bf2ee43fa883cf7b6733503
AIejoCastro <alejocastro20@hotmail.com>
2024-11-07 17:20:44 -0500

    PsychoApp

---
+++ psychoApp/expertSystem.py
@@ -0,0 +1,274 @@
+import numpy as np
+from pgmpy.models import BayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+
+# Definir la estructura de la red Bayesiana
+model = BayesianNetwork([
+    ('Panico', 'hiperventilacion'),
+    ('Panico', 'llanto_incontenible'),
+    ('Panico', 'agresividad'),
+    ('Panico', 'abuso_de_sustancias'),
+    ('Panico', 'distorsion_de_la_realidad'),
+    ('Panico', 'ideas_de_autolesion'),
+    ('Panico', 'temblores'),
+    ('Panico', 'sudoracion'),
+    ('Panico', 'pitido'),
+    ('Panico', 'nubla_de_vision'),
+    ('Panico', 'palpitaciones_rapidas'),
+    ('Panico', 'vomito'),
+    ('Panico', 'abstinencia'),
+    ('Panico', 'agresion_a_otros'),
+    ('Ansiedad', 'hiperventilacion'),
+    ('Ansiedad', 'llanto_incontenible'),
+    ('Ansiedad', 'agresividad'),
+    ('Ansiedad', 'abuso_de_sustancias'),
+    ('Ansiedad', 'distorsion_de_la_realidad'),
+    ('Ansiedad', 'ideas_de_autolesion'),
+    ('Ansiedad', 'temblores'),
+    ('Ansiedad', 'sudoracion'),
+    ('Ansiedad', 'pitido'),
+    ('Ansiedad', 'nubla_de_vision'),
+    ('Ansiedad', 'palpitaciones_rapidas'),
+    ('Ansiedad', 'vomito'),
+    ('Ansiedad', 'abstinencia'),
+    ('Ansiedad', 'agresion_a_otros'),
+    ('Psicotico', 'hiperventilacion'),
+    ('Psicotico', 'llanto_incontenible'),
+    ('Psicotico', 'agresividad'),
+    ('Psicotico', 'abuso_de_sustancias'),
+    ('Psicotico', 'distorsion_de_la_realidad'),
+    ('Psicotico', 'ideas_de_autolesion'),
+    ('Psicotico', 'temblores'),
+    ('Psicotico', 'sudoracion'),
+    ('Psicotico', 'pitido'),
+    ('Psicotico', 'nubla_de_vision'),
+    ('Psicotico', 'palpitaciones_rapidas'),
+    ('Psicotico', 'vomito'),
+    ('Psicotico', 'abstinencia'),
+    ('Psicotico', 'agresion_a_otros')
+])
+
+
+# Definiciones de las CPDs para los estados de 'Pánico', 'Ansiedad' y 'Psicótico'
+cpd_panico = TabularCPD(variable='Panico', variable_card=2, values=[[0.7], [0.3]])
+cpd_ansiedad = TabularCPD(variable='Ansiedad', variable_card=2, values=[[0.6], [0.4]])
+cpd_psicotico = TabularCPD(variable='Psicotico', variable_card=2, values=[[0.8], [0.2]])
+
+# Definiciones de las CPDs para los síntomas ajustadas
+
+cpd_hiperventilacion = TabularCPD(variable='hiperventilacion', variable_card=2,
+                                  values=[[0.7, 0.4, 0.6, 0.3, 0.5, 0.8, 0.4, 0.3],
+                                          [0.3, 0.6, 0.4, 0.7, 0.5, 0.2, 0.6, 0.7]],
+                                  evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                                  evidence_card=[2, 2, 2])
+
+cpd_llanto = TabularCPD(variable='llanto_incontenible', variable_card=2,
+                        values=[[0.8, 0.6, 0.7, 0.4, 0.5, 0.7, 0.4, 0.2],
+                                [0.2, 0.4, 0.3, 0.6, 0.5, 0.3, 0.6, 0.8]],
+                        evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                        evidence_card=[2, 2, 2])
+
+cpd_agresividad = TabularCPD(variable='agresividad', variable_card=2,
+                              values=[[0.4, 0.3, 0.6, 0.5, 0.4, 0.6, 0.5, 0.3],
+                                      [0.6, 0.7, 0.4, 0.5, 0.6, 0.4, 0.5, 0.7]],
+                              evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                              evidence_card=[2, 2, 2])
+
+cpd_abuso_sustancias = TabularCPD(variable='abuso_de_sustancias', variable_card=2,
+                                  values=[[0.5, 0.3, 0.7, 0.4, 0.5, 0.6, 0.3, 0.2],
+                                          [0.5, 0.7, 0.3, 0.6, 0.5, 0.4, 0.7, 0.8]],
+                                  evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                                  evidence_card=[2, 2, 2])
+
+cpd_distorsion_realidad = TabularCPD(variable='distorsion_de_la_realidad', variable_card=2,
+                                     values=[[0.6, 0.4, 0.7, 0.3, 0.4, 0.8, 0.4, 0.2],
+                                             [0.4, 0.6, 0.3, 0.7, 0.6, 0.2, 0.6, 0.8]],
+                                     evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                                     evidence_card=[2, 2, 2])
+
+cpd_ideas_autolesion = TabularCPD(variable='ideas_de_autolesion', variable_card=2,
+                                  values=[[0.5, 0.3, 0.6, 0.4, 0.3, 0.7, 0.4, 0.2],
+                                          [0.5, 0.7, 0.4, 0.6, 0.7, 0.3, 0.6, 0.8]],
+                                  evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                                  evidence_card=[2, 2, 2])
+
+cpd_temblores = TabularCPD(variable='temblores', variable_card=2,
+                           values=[[0.6, 0.4, 0.7, 0.3, 0.5, 0.7, 0.3, 0.2],
+                                   [0.4, 0.6, 0.3, 0.7, 0.5, 0.3, 0.7, 0.8]],
+                           evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                           evidence_card=[2, 2, 2])
+
+cpd_sudoracion = TabularCPD(variable='sudoracion', variable_card=2,
+                            values=[[0.7, 0.5, 0.8, 0.4, 0.6, 0.9, 0.5, 0.3],
+                                    [0.3, 0.5, 0.2, 0.6, 0.4, 0.1, 0.5, 0.7]],
+                            evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                            evidence_card=[2, 2, 2])
+
+cpd_pitido = TabularCPD(variable='pitido', variable_card=2,
+                         values=[[0.5, 0.3, 0.6, 0.4, 0.4, 0.7, 0.3, 0.2],
+                                 [0.5, 0.7, 0.4, 0.6, 0.6, 0.3, 0.7, 0.8]],
+                         evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                         evidence_card=[2, 2, 2])
+
+cpd_nubla_vision = TabularCPD(variable='nubla_de_vision', variable_card=2,
+                               values=[[0.6, 0.4, 0.7, 0.3, 0.5, 0.8, 0.4, 0.2],
+                                       [0.4, 0.6, 0.3, 0.7, 0.5, 0.2, 0.6, 0.8]],
+                               evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                               evidence_card=[2, 2, 2])
+
+cpd_palpitaciones = TabularCPD(variable='palpitaciones_rapidas', variable_card=2,
+                               values=[[0.7, 0.5, 0.8, 0.4, 0.6, 0.9, 0.5, 0.3],
+                                       [0.3, 0.5, 0.2, 0.6, 0.4, 0.1, 0.5, 0.7]],
+                               evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                               evidence_card=[2, 2, 2])
+
+cpd_vomito = TabularCPD(variable='vomito', variable_card=2,
+                         values=[[0.5, 0.3, 0.6, 0.4, 0.4, 0.7, 0.3, 0.2],
+                                 [0.5, 0.7, 0.4, 0.6, 0.6, 0.3, 0.7, 0.8]],
+                         evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                         evidence_card=[2, 2, 2])
+
+cpd_abstinencia = TabularCPD(variable='abstinencia', variable_card=2,
+                              values=[[0.6, 0.4, 0.7, 0.3, 0.5, 0.8, 0.4, 0.2],
+                                      [0.4, 0.6, 0.3, 0.7, 0.5, 0.2, 0.6, 0.8]],
+                              evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                              evidence_card=[2, 2, 2])
+
+cpd_agresion_otros = TabularCPD(variable='agresion_a_otros', variable_card=2,
+                                values=[[0.7, 0.5, 0.8, 0.4, 0.6, 0.9, 0.5, 0.3],
+                                        [0.3, 0.5, 0.2, 0.6, 0.4, 0.1, 0.5, 0.7]],
+                                evidence=['Panico', 'Ansiedad', 'Psicotico'],
+                                evidence_card=[2, 2, 2])
+
+# Agregar todas las CPDs al modelo
+model.add_cpds(cpd_hiperventilacion, cpd_llanto, cpd_agresividad, cpd_abuso_sustancias,
+               cpd_distorsion_realidad, cpd_ideas_autolesion, cpd_temblores, cpd_sudoracion,
+               cpd_pitido, cpd_nubla_vision, cpd_palpitaciones, cpd_vomito, cpd_abstinencia,
+               cpd_agresion_otros, cpd_panico, cpd_ansiedad, cpd_psicotico)
+
+# Verificar que el modelo está bien definido
+assert model.check_model()
+
+# Crear el objeto de inferencia
+inference = VariableElimination(model)
+
+from experta import *
+
+# Definir las clases de hechos
+class Symptom(Fact):
+    hiperventilacion = Field(int, default=0)
+    llanto_incontenible = Field(int, default=0)
+    agresividad = Field(int, default=0)
+    abuso_de_sustancias = Field(int, default=0)
+    distorsion_de_la_realidad = Field(int, default=0)
+    ideas_de_autolesion = Field(int, default=0)
+    temblores = Field(int, default=0)
+    sudoracion = Field(int, default=0)
+    pitido = Field(int, default=0)
+    nubla_de_vision = Field(int, default=0)
+    palpitaciones_rapidas = Field(int, default=0)
+    vomito = Field(int, default=0)
+    abstinencia = Field(int, default=0)
+    agresion_a_otros = Field(int, default=0)
+
+class Diagnosis(Fact):
+    """Diagnóstico basado en síntomas"""
+    pass
+
+# Definir el sistema experto
+class PsychologicalEmergencyExpert(KnowledgeEngine):
+
+    def __init__(self):
+       super().__init__()
+       self.result = ""
+
+    def infer_diagnosis_generic(self, f):
+        # Realizar inferencia con el modelo Bayesiano
+        symptoms = {
+            'hiperventilacion': f['hiperventilacion'], 'llanto_incontenible': f['llanto_incontenible'],
+            'agresividad': f['agresividad'], 'abuso_de_sustancias': f['abuso_de_sustancias'],
+            'distorsion_de_la_realidad': f['distorsion_de_la_realidad'], 'ideas_de_autolesion': f['ideas_de_autolesion'],
+            'temblores': f['temblores'], 'sudoracion': f['sudoracion'], 'pitido': f['pitido'],
+            'nubla_de_vision': f['nubla_de_vision'], 'palpitaciones_rapidas': f['palpitaciones_rapidas'],
+            'vomito': f['vomito'], 'abstinencia': f['abstinencia'], 'agresion_a_otros': f['agresion_a_otros']
+        }
+
+        # Construir el diccionario de evidencia
+        evidence = {key: value for key, value in symptoms.items() if value == 1}
+
+        result_anxiety = inference.query(variables=['Ansiedad'], evidence=evidence)
+        result_panic = inference.query(variables=['Panico'], evidence=evidence)
+        result_psicotic = inference.query(variables=['Psicotico'], evidence=evidence)
+
+        results = [result_anxiety.values[1], result_panic.values[1], result_psicotic.values[1]]
+        disease_results = [result_anxiety.variables, result_panic.variables, result_psicotic.variables]
+
+        disease_probability = encontrar_maximo(results)
+        disease = disease_results[results.index(disease_probability)]
+
+        self.declare(Diagnosis(name=disease[0]))
+
+    @Rule(AS.f << Symptom(hiperventilacion=MATCH.hiperventilacion,
+        llanto_incontenible=MATCH.llanto_incontenible,
+        agresividad=MATCH.agresividad,
+        abuso_de_sustancias=MATCH.abuso_de_sustancias,
+        distorsion_de_la_realidad=MATCH.distorsion_de_la_realidad,
+        ideas_de_autolesion=MATCH.ideas_de_autolesion,
+        temblores=MATCH.temblores,
+        sudoracion=MATCH.sudoracion,
+        pitido=MATCH.pitido,
+        nubla_de_vision=MATCH.nubla_de_vision,
+        palpitaciones_rapidas=MATCH.palpitaciones_rapidas,
+        vomito=MATCH.vomito,
+        abstinencia=MATCH.abstinencia,
+        agresion_a_otros=MATCH.agresion_a_otros))
+    def infer_diagnosis_4(self, f):
+        self.infer_diagnosis_generic(f)
+
+    @Rule(Diagnosis(name = "Ansiedad"))
+    def anxiety(self):
+      self.result += f"Si te sientes abrumado, busca un lugar tranquilo y observa 5 cosas a tu alrededor, enfocándote en los detalles. Cierra los ojos (si te sientes cómodo) y escucha 4 sonidos distintos. Toca 3 objetos cercanos, sintiendo su textura, temperatura y forma. Respira hondo por la nariz (4 segundos), retén (4 segundos) y exhala por la boca (6 segundos). Repite 5 veces. Presta atención mientras respiras a lo que ves, oyes y tocas, manteniéndote presente. Recuerda, está bien tomarte un tiempo para ti. Este ejercicio te ayuda a encontrar paz y claridad. Respira y tómate tu tiempo."
+
+    @Rule(Diagnosis(name = "Panico"))
+    def panic(self):
+      self.result += f"Debes respirar conscientemente, intenta contar hasta 10 y luego regresa a contar hacia atras, busca un lugar tranquilo y seguro. Trata de no estar solo, llama a alguien de confianza."
+
+    @Rule(Diagnosis(name = "Psicotico"))
+    def suicidal(self):
+      self.result += f"Debes llamar a un numero de emergencia o a alguien de tu entera confianza, entiende que tu vida vale mucho"
+
+# Crear la instancia del sistema experto
+engine = PsychologicalEmergencyExpert()
+
+def encontrar_maximo(array):
+  maximo = array[0]
+  for elemento in array:
+    if elemento > maximo:
+      maximo = elemento
+  return maximo
+
+# Tu método start del sistema experto
+def start(hiper, llanto, agresividad, abuso, distorsion, ideas, temblores, sudoracion, pitido, nubla, palpitaciones, vomito, abstinencia, agresion):
+    symptoms_values = {
+        'hiperventilacion': hiper,
+        'llanto_incontenible': llanto,
+        'agresividad': agresividad,
+        'abuso_de_sustancias': abuso,
+        'distorsion_de_la_realidad': distorsion,
+        'ideas_de_autolesion': ideas,
+        'temblores': temblores,
+        'sudoracion': sudoracion,
+        'pitido': pitido,
+        'nubla_de_vision': nubla,
+        'palpitaciones_rapidas': palpitaciones,
+        'vomito': vomito,
+        'abstinencia': abstinencia,
+        'agresion_a_otros': agresion
+    }
+    engine.reset()
+    engine.result = ""
+    engine.declare(Symptom(**symptoms_values))
+    engine.run()
+
+    return {'resultado': engine.result}
---
+++ psychoApp/views.py
@@ -0,0 +1,129 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.csrf import csrf_exempt
+import psychoApp.expertSystem as expertSystem
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, RegisterForm
+from django.contrib.auth.decorators import login_required
+from .models import HistorialResultado
+
+@login_required
+def index(request):
+    doc_index = open("psychoApp/templates/index.html")
+    template = Template(doc_index.read())
+    doc_index.close()
+    context = Context()
+    doc = template.render(context)
+    return HttpResponse(doc)
+
+@login_required
+def about(request):
+    doc_index = open("psychoApp/templates/about.html")
+    template = Template(doc_index.read())
+    doc_index.close()
+    context = Context()
+    doc = template.render(context)
+    return HttpResponse(doc)
+
+@login_required
+def chat(request):
+    doc_index = open("psychoApp/templates/chat.html")
+    template = Template(doc_index.read())
+    doc_index.close()
+    context = Context()
+    doc = template.render(context)
+    return HttpResponse(doc)
+
+@login_required
+@csrf_exempt
+def process_symptoms(request):
+    if request.method == 'POST':
+        selected_buttons = request.POST.get('selected_buttons').split(',')  # Split the comma-separated values
+        symptoms_dict = {
+            'hiperventilacion': 1 if '1' in selected_buttons else 0,
+            'llanto_incontenible': 1 if '2' in selected_buttons else 0,
+            'agresividad': 1 if '3' in selected_buttons else 0,
+            'abuso_de_sustancias': 1 if '4' in selected_buttons else 0,
+            'distorsion_de_la_realidad': 1 if '5' in selected_buttons else 0,
+            'ideas_de_autolesion': 1 if '6' in selected_buttons else 0,
+            'temblores': 1 if '7' in selected_buttons else 0,
+            'sudoracion': 1 if '8' in selected_buttons else 0,
+            'pitido': 1 if '9' in selected_buttons else 0,
+            'nubla_de_vision': 1 if '10' in selected_buttons else 0,
+            'palpitaciones_rapidas': 1 if '11' in selected_buttons else 0,
+            'vomito': 1 if '12' in selected_buttons else 0,
+            'abstinencia': 1 if '13' in selected_buttons else 0,
+            'agresion_a_otros': 1 if '14' in selected_buttons else 0
+        }
+
+        # Llama al método del sistema experto y captura el resultado
+        result = expertSystem.start(
+            symptoms_dict['hiperventilacion'],
+            symptoms_dict['llanto_incontenible'],
+            symptoms_dict['agresividad'],
+            symptoms_dict['abuso_de_sustancias'],
+            symptoms_dict['distorsion_de_la_realidad'],
+            symptoms_dict['ideas_de_autolesion'],
+            symptoms_dict['temblores'],
+            symptoms_dict['sudoracion'],
+            symptoms_dict['pitido'],
+            symptoms_dict['nubla_de_vision'],
+            symptoms_dict['palpitaciones_rapidas'],
+            symptoms_dict['vomito'],
+            symptoms_dict['abstinencia'],
+            symptoms_dict['agresion_a_otros']
+        )
+
+        # Guardar el resultado en el historial
+        historial = HistorialResultado(
+            usuario=request.user,
+            sintomas=','.join(selected_buttons),
+            resultado=result['resultado']
+        )
+        historial.save()
+
+        # Pasa el resultado a la plantilla
+        return render(request, 'result_page.html', {'selected_buttons': selected_buttons, 'result': result})
+    
+    return render(request, 'your_template.html')
+
+@login_required
+def ver_historial(request):
+    historial = HistorialResultado.objects.filter(usuario=request.user).order_by('-fecha')
+    return render(request, 'see_history.html', {'historial': historial})
+
+@login_required
+def eliminar_historial(request, id):
+    if request.method == "POST":
+        entry = get_object_or_404(HistorialResultado, id=id)
+        entry.delete()
+        return redirect('ver_historial')
+    return HttpResponse(status=405)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home')  
+            else:
+                form.add_error(None, 'Invalid username or password')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('login') 
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
